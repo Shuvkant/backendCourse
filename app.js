2dabb5379fe2d0e272aac43ff90721ddb0c1f025
Shuvkant <weird426344@gmail.com>
2025-07-27 13:38:01 +0545

    setup config

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from "express"
-
+import { PORT } from "./config/env.js"
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Welcome to the backend course")
+  res.send("Welcome to the backend course.")
 })
 
-app.listen(3000, () => {
-  console.log("App is running in port 3000")
+app.listen(PORT, () => {
+  console.log(`App is running in port ${PORT}`)
 })
 
 export default app
