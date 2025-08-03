d9d355fec58a70762bddeda6ae589ae16fbb5dba
Shuvkant <weird426344@gmail.com>
2025-08-04 00:49:16 +0545

    completed authntication

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-import express, { urlencoded } from "express"
-import { PORT } from "./config/env.js"
+import express, { urlencoded } from 'express'
+import { PORT } from './config/env.js'
 
-import authRouter from "./routes/auth.routes.js"
-import userRouter from "./routes/user.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
-import connectToDatabase from "./database/mongodb.js"
-import errorMiddlewares from "./middlewares/error.middleware.js"
-import cookieParser from "cookie-parser"
+import authRouter from './routes/auth.routes.js'
+import userRouter from './routes/user.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
+import connectToDatabase from './database/mongodb.js'
+import errorMiddlewares from './middlewares/error.middleware.js'
+import cookieParser from 'cookie-parser'
+// import { JWT_Expires_In, JWT_SECRET } from '../config/env.js'
+import { JWT_EXPIRES_IN, JWT_SECRET } from './config/env.js'
 
-const app = express();
+const app = express()
 
+app.use(express.json())
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscription', subscriptionRouter)
 app.use(errorMiddlewares)
-app.use(express.json())
-app.use(urlencoded({ extended: false }))
+app.use(urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
-  res.send("Welcome to the backend course. Shuvkant Chaudhary Phanait")
+  res.send('Welcome to the backend course. Shuvkant Chaudhary Phanait')
 })
 app.get('/learning/', (req, res) => {
   res.send(req.body)
@@ -27,8 +29,9 @@ app.get('/learning/', (req, res) => {
 
 app.listen(PORT, async () => {
   console.log(`App is running in port  http://localhost:${PORT}`)
+  console.log(`JWT_Expires_In: ${JWT_EXPIRES_IN}`)
+  console.log(`JWT_SECRET: ${JWT_SECRET}`)
   await connectToDatabase()
-
 })
 
 export default app
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,44 +1,42 @@
-import mongoose from "mongoose"
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
-import User from "../models/user.model.js";
-import { JWT_Expires_In, JWT_SECRET } from "../config/env.js";
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import User from '../models/user.model.js'
+import { JWT_EXPIRES_IN, JWT_SECRET } from '../config/env.js'
 export const signUp = async (req, res, next) => {
   const session = await mongoose.startSession()
   session.startTransaction()
   try {
     // Logic for signing up a user
-    if (!req.body || typeof req.body !== "object") {
-      const err = new Error("Request body is required");
-      err.statusCode = 400;
-      throw err;
-    }
-    const { name, email, password } = req.body;
+    const { name, email, password } = req.body
     //Check if the user already exists
     const existingUser = await User.findOne({ email })
     if (existingUser) {
-      const error = new Error("user already exits")
-      error.statusCode = 409;
+      const error = new Error('user already exits')
+      error.statusCode = 409
       throw error
     }
 
     // hasing the passwrod
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
-    const newUsers = await User.create([{ name, email, password: hashedPassword }], { session })
-    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, { expiresIn: JWT_Expires_In })
+    const newUsers = await User.create(
+      [{ name, email, password: hashedPassword }],
+      { session }
+    )
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    })
     await session.commitTransaction()
     session.endSession()
 
     res.status(201).json({
       success: true,
-      message: "User created successfully",
+      message: 'User created successfully',
       data: {
         token,
         user: newUsers[0],
-      }
-
-
+      },
     })
   } catch (error) {
     await session.abortTransaction()
@@ -48,8 +46,39 @@ export const signUp = async (req, res, next) => {
 }
 
 export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const error = new Error("user not found");
+      error.statusCode = 404;
+      throw error
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+      const error = new Error("Invalid email or password")
+      error.statusCode = 401
+      throw error
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    })
+
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      data: {
+        token,
+        user
+      }
+    })
+
+  } catch (error) {
+    next(error)
+  }
 }
-export const signOut = async (req, res, next) => {
-}
-export const refreshToken = async (req, res, next) => {
-}
+export const signOut = async (req, res, next) => { }
+export const refreshToken = async (req, res, next) => { }
