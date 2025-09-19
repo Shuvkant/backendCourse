5a9d751a4420f2ad49c4be16e59e16c97317b668
Shuvkant <weird426344@gmail.com>
2025-09-19 23:37:28 +0545

    arcjet implemented

--- app.js
+++ app.js
@@ -9,16 +9,20 @@ import errorMiddlewares from './middlewares/error.middleware.js'
 import cookieParser from 'cookie-parser'
 // import { JWT_Expires_In, JWT_SECRET } from '../config/env.js'
 import { JWT_EXPIRES_IN, JWT_SECRET } from './config/env.js'
+import arcjetMiddleware from './middlewares/arcjet.middleware.js'
 
 const app = express()
 
 app.use(express.json())
+app.use(urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(arcjetMiddleware)
+
+
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscription', subscriptionRouter)
 app.use(errorMiddlewares)
-app.use(urlencoded({ extended: true }))
-app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('Welcome to the backend course. Shuvkant Chaudhary Phanait. Its 3rd Ashoj')
